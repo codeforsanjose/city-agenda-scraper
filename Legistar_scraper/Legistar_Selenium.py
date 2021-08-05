babf7b7738ab3b2fc79c818aa16dd1a790d64d4e
Monsterblader <Monsterblader@gmail.com>
2021-08-04 22:04:38 -0700

    Opens meeting links and clicks on Memoranda. Checks for missing and empty links.

--- Legistar_scraper/Legistar_Selenium.py
+++ Legistar_scraper/Legistar_Selenium.py
@@ -17,77 +17,124 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 import pandas as pd
-from dotenv import load_dotenv
 
-load_dotenv()
-full_path = os.environ.get('full_path')
-current_city = sys.argv[1]
-time_period = sys.argv[2]
+def scrape_meetings(city_name="sanjose", time_period="Last Month", target_meeting="City Council"):
 
-session = HTMLSession()
+    driver = webdriver.Chrome()
+    actions = ActionChains(driver)
 
-
-def set_date(start_date=date.today(), end_date=date.today() + timedelta(days=7)):
-    if start_date.month >= date.today().month:
-        return 4
-# 4 should be the right number of spaces from "last year"
-
-#def set_committee(committee='City Council'):
-
-
-def scrape_meetings(url):
-
-    driver = webdriver.Firefox()
-    driver.get("https://%s.legistar.com/Calendar.aspx" %(url))
-    WebDriverWait(driver, 10000).until(EC.presence_of_element_located(
+    driver.get("https://%s.legistar.com/Calendar.aspx" %city_name)
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located(
         (By.ID, 'ctl00_ContentPlaceHolder1_tdYears')))
+
     driver.find_element_by_id('ctl00_ContentPlaceHolder1_tdYears').click()
     dates = driver.find_element_by_id('ctl00_ContentPlaceHolder1_lstYears_DropDown')
     for val in dates.find_elements_by_tag_name('li'):
         if val.text == time_period:
             val.click()
             break
-    WebDriverWait(driver, 10000).until(EC.presence_of_element_located(
+
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+        (By.ID, 'ctl00_ContentPlaceHolder1_lstBodies_Input')))
+# need to scroll element into view.
+
+    meetings_dropdown = driver.find_element_by_id('ctl00_ContentPlaceHolder1_lstBodies_Input')
+    actions.move_to_element(meetings_dropdown).perform()
+    meetings_dropdown.click()
+    menu_items = driver.find_element_by_id('ctl00_ContentPlaceHolder1_lstBodies_DropDown')
+    for item in menu_items.find_elements_by_tag_name('li'):
+        if item.text == target_meeting:
+            item.click()
+            break
+
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located(
         (By.ID, 'ctl00_ContentPlaceHolder1_gridCalendar_ctl00')))
-    for element in driver.find_elements_by_link_text('Meeting details'):
+
+    target_range_table = driver.find_element_by_id('ctl00_ContentPlaceHolder1_gridCalendar_ctl00')
+    for element in target_range_table.find_elements_by_link_text('Meeting details'):
         link = element.get_attribute('href')
         if link is not None:
-            get_agenda(link)
+            driver.execute_script(
+                '''window.open("%s", "_blank");''' %link)
+            WebDriverWait(driver, 20).until(EC.number_of_windows_to_be(2))
+            driver.switch_to_window(driver.window_handles[1])
+            WebDriverWait(driver, 60).until(EC.presence_of_all_elements_located(
+                (By.ID, 'ctl00_ContentPlaceHolder1_gridMain_ctl00')))
+
+# NoSuchElementException when "No records to display" - convert to try-finally-continue?
+            # agenda_links = filter(lambda el: el.get_attribute('href') is not None,
+            #     map(lambda el: el.find_element_by_tag_name('td a'),
+            #         driver.find_element_by_id('ctl00_ContentPlaceHolder1_gridMain_ctl00').find_elements_by_tag_name('tbody')[0].find_elements_by_tag_name('tr')))
+
+            meeting_rows = driver.find_element_by_id('ctl00_ContentPlaceHolder1_gridMain_ctl00').find_elements_by_tag_name('tbody')[0].find_elements_by_tag_name('tr')
+            try:
+                def get_a(el):
+                    try:
+                        return el.find_element_by_tag_name('td a')
+                    except:
+                        return None
+
+                meeting_links = map(get_a, meeting_rows)
+                def get_href(el):
+                    if el is not None:
+                        return el.get_attribute('href')
+                    else:
+                        return None
+
+                agenda_links = filter(get_href, meeting_links)
+
+                for agenda_link in agenda_links:
+                    driver.execute_script(
+                        '''window.open("%s", "_blank");''' %agenda_link.get_attribute('href'))
+                    WebDriverWait(driver, 20).until(EC.number_of_windows_to_be(3))
+                    driver.switch_to_window(driver.window_handles[2])
+                    try:
+                        # TODO change to link text contains memorandum?
+                        memorandum = driver.find_element_by_link_text('Memorandum')
+                        memorandum.click()
+                    finally:
+                        driver.close()
+                        driver.switch_to_window(driver.window_handles[1])
+                        continue
+
+            finally:
+                driver.close()
+                driver.switch_to_window(driver.window_handles[0])
 
     driver.close()
 
-#    pd.read_html(driver.page_source) could be used to take tables in Selenium instead of requests_html
 
 def get_agenda(link):
-    print(link)
+    session = HTMLSession()
 
-    header_list = [ 'File #', 'Staff Report link', 'Ver.', 'Agenda #', 'Agenda Note', 'Type', 'Title', 'Action', 'Result', 'Action Details', 'Video' ]
 # this is a revised list of headers for the output csv file.
-
+    header_list = [ 'File #', 'Staff Report link', 'Ver.', 'Agenda #', 'Agenda Note', 'Type', 'Title', 'Action', 'Result', 'Action Details', 'Video' ]
     tables = pd.read_html(link, keep_default_na=False)
 
-    meeting_name = tables[2].iloc[0,1].replace('/', '_')
 # Grabbing meeting name from tables
-    target_date = (tables[3].iloc[0,1]).split(' ')[0].replace('/', '-')
+    meeting_name = tables[2].iloc[0,1].replace('/', '_')
 # Grabbing meeting date from tables
+    target_date = (tables[3].iloc[0,1]).split(' ')[0].replace('/', '-')
     last_table = len(tables) - 1
-    tables[last_table] = tables[last_table].reindex(columns = header_list)
 # This adds a new column to dataframe for the 'Staff Report link'
+    tables[last_table] = tables[last_table].reindex(columns = header_list)
 
-    l = tables[last_table]['File #'].tolist()
 # Grabs column of File # to find Staff Report hyperlinks
+    l = tables[last_table]['File #'].tolist()
     data = []
-    r2 = session.get(link)
 # Loads Legistar 'Meeting Details' page
+    r2 = session.get(link)
 
     for item in l:
+# this checks for blank rows with no File # (Staff Report)
         if not item:
-            # this checks for blank rows with no File # (Staff Report)
             data.append('')
         else:
-            report = r2.html.find('a', containing=str(item), first=True)
 # Finds hyperlinks from anchor tags
+            report = r2.html.find('a', containing=str(item), first=True)
             try:
                 data.append(report.attrs.get('href'))
             except:
@@ -95,8 +142,24 @@ def get_agenda(link):
                 continue
 
     tables[last_table]['Staff Report link'] = data
-    tables[last_table].to_csv((full_path + meeting_name + '_' + target_date + '.csv'), index=False, errors='replace')
 # Creates filename with 'Meeting Name' and 'Date'
+    tables[last_table].to_csv((full_path + meeting_name + '_' + target_date + '.csv'), index=False, errors='replace')
 
+if __name__ == "__main__":
+    from dotenv import load_dotenv
 
-scrape_meetings(current_city)
+    # if len(sys.argv) < 3:
+    #     print("""To run this file properly, please include the following arguments:
+    #         The city-name part of the Legistar URL.  i.e. https://<city name>.legistar.com.
+    #         The desired time period.  It can be one of the following: All Years, Last Year, Last Month, Last Week, This Year, This Month, This Week, Today, Next Week, Next Month, Next Year.
+    #     """)
+
+    # else:
+
+    load_dotenv()
+    full_path = os.environ.get('full_path')
+    current_city = sys.argv[1]
+    time_period = sys.argv[2]
+    target_meeting = sys.argv[3]
+
+    scrape_meetings(current_city, time_period, target_meeting)
