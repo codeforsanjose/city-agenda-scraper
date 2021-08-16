3b44a06b22ba4d1049e820117fc6905a5fec6caa
krammy19 <marknoack9@gmail.com>
2021-08-15 20:10:26 -0700

    Legistar update
    - Fixed bug on scraping meeting date / type
    - Made scraper more flexible for different Legistar formats

--- Legistar_scraper/Legistar_Selenium.py
+++ Legistar_scraper/Legistar_Selenium.py
@@ -21,9 +21,8 @@ import pandas as pd
 from dotenv import load_dotenv
 from Gdrive_upload_methods import drive_upload, drive_launch
 
-
-drive = drive_launch()
 full_path = os.environ.get('full_path')
+drive = drive_launch()
 current_city = sys.argv[1]
 time_period = sys.argv[2]
 meeting_type = sys.argv[3]
@@ -54,14 +53,40 @@ def scrape_meetings(url):
     WebDriverWait(driver, 10000).until(EC.presence_of_element_located(
         (By.ID, 'ctl00_ContentPlaceHolder1_gridCalendar_ctl00')))    
     All_Meetings =  driver.find_element_by_id('ctl00_ContentPlaceHolder1_divGrid') 
+    rows = All_Meetings.find_elements_by_tag_name('tr') 
+    Name_index = 0
+    Meeting_Date_index = 1
+    Meeting_Time_index = 2
+    Meeting_Details_index = 4
+    i = 0
+    for header in rows[0].find_elements_by_tag_name('th'):
+        if header.text == "Name":   
+            Name_index = i
+            print('Name found')
+        if header.text == "Meeting Date ":
+            Meeting_Date_index = i
+            print('MD found')
+        if header.text == "Meeting Time": 
+            Meeting_Time_index = i
+            print('MT')
+        if header.text == "Meeting Details": 
+            Meeting_Details_index = i
+            print('MDetails')
+        i += 1
+    for row in rows[1:]: 
+        cells = row.find_elements_by_tag_name('td')
+        Name = cells[Name_index].text
+        Meeting_Date = (cells[Meeting_Date_index].text).replace('/', '-')
+        Meeting_Time = cells[Meeting_Time_index].text
+        link = cells[Meeting_Details_index].find_element_by_link_text('Meeting details').get_attribute('href')
 
-    for element in All_Meetings.find_elements_by_link_text('Meeting details'): 
-        link = element.get_attribute('href')
         if link is not None:
-            get_agenda(link)
+   
+   
+            get_agenda(link, Meeting_Date, Name)
     driver.close()
 
-def get_agenda(link):
+def get_agenda(link, meeting_date, meeting_name):
     print(link)
 
     header_list = [ 'File #', 'Date', 'Meeting type', 'Staff Report link', 'Ver.', 'Agenda #', 'Agenda Note', 'Type', 'Title', 'Action', 'Result', 'Action Details', 'Video' ]
@@ -70,13 +95,9 @@ def get_agenda(link):
     tables = pd.read_html(link, keep_default_na=False)
     # sometimes generates ConnectionResetError
 
-    meeting_name = tables[1].iloc[0,1].replace('/', '_')
-# Grabbing meeting name from tables (for San Jose, tables[1] works, but need to make this dynamic for other cities with different table indexes)
-    target_date = (tables[2].iloc[0,1]).split(' ')[0].replace('/', '-')
-# Grabbing meeting date from tables (for San Jose, tables[2] works, but need to make this dynamic for other cities with different table indexes)
     last_table = len(tables) - 1
     agenda_table = tables[last_table].reindex(columns = header_list)
-    agenda_table['Date'] = target_date
+    agenda_table['Date'] = meeting_date
     agenda_table['Meeting type'] = meeting_name
 # This adds new columns to dataframe for the 'Staff Report link', 'Date' and 'Meeting type'
     agenda_table = agenda_table[agenda_table['File #'] != '']
@@ -101,7 +122,7 @@ def get_agenda(link):
                 continue
 
     agenda_table['Staff Report link'] = data
-    agenda_table.to_csv((full_path + meeting_name + '_' + target_date + '.csv'), index=False, errors='replace')
+    agenda_table.to_csv((full_path + meeting_name + '_' + meeting_date + '.csv'), index=False, errors='replace')
 # Creates filename with 'Meeting Name' and 'Date'
     drive_upload(full_path, drive)
 
