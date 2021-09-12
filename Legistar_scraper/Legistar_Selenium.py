2094653478d41a2c02d6124bdc71f0fea0612b92
dntrply <dntrplytch@gmail.com>
2021-09-12 16:59:04 -0700

    * Load environment * Lauch Firefox in headless mode * Run Use your own gmail account and use the shared drive Cities * Note - using the Agenda Scraper account directly will no longer work

--- Legistar_scraper/Legistar_Selenium.py
+++ Legistar_scraper/Legistar_Selenium.py
@@ -17,12 +17,14 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 import pandas as pd
 from dotenv import load_dotenv
 from Gdrive_upload_methods import drive_upload, drive_launch
 
-full_path = os.environ.get('full_path')
+load_dotenv()
 drive = drive_launch()
+full_path = os.environ.get('full_path')
 current_city = sys.argv[1]
 time_period = sys.argv[2]
 meeting_type = sys.argv[3]
@@ -30,10 +32,12 @@ meeting_type = sys.argv[3]
 session = HTMLSession()
 
 
-def scrape_meetings(url):
+def get_link():
+    options = Options()
+    options.headless = True
 
-    driver = webdriver.Firefox()
-    driver.get("https://%s.legistar.com/Calendar.aspx" %(url))
+    driver = webdriver.Firefox(options=options)
+    driver.get("https://%s.legistar.com/Calendar.aspx" %(current_city))
     WebDriverWait(driver, 10000).until(EC.presence_of_element_located(
         (By.ID, 'ctl00_ContentPlaceHolder1_tdYears')))
     driver.find_element_by_id('ctl00_ContentPlaceHolder1_tdYears').click()
@@ -53,51 +57,28 @@ def scrape_meetings(url):
     WebDriverWait(driver, 10000).until(EC.presence_of_element_located(
         (By.ID, 'ctl00_ContentPlaceHolder1_gridCalendar_ctl00')))    
     All_Meetings =  driver.find_element_by_id('ctl00_ContentPlaceHolder1_divGrid') 
-    rows = All_Meetings.find_elements_by_tag_name('tr') 
-    Name_index = 0
-    Meeting_Date_index = 1
-    Meeting_Time_index = 2
-    Meeting_Details_index = 4
-    i = 0
-    for header in rows[0].find_elements_by_tag_name('th'):
-        if header.text == "Name":   
-            Name_index = i
-            print('Name found')
-        if header.text == "Meeting Date ":
-            Meeting_Date_index = i
-            print('MD found')
-        if header.text == "Meeting Time": 
-            Meeting_Time_index = i
-            print('MT')
-        if header.text == "Meeting Details": 
-            Meeting_Details_index = i
-            print('MDetails')
-        i += 1
-    for row in rows[1:]: 
-        cells = row.find_elements_by_tag_name('td')
-        Name = cells[Name_index].text
-        Meeting_Date = (cells[Meeting_Date_index].text).replace('/', '-')
-        Meeting_Time = cells[Meeting_Time_index].text
-        link = cells[Meeting_Details_index].find_element_by_link_text('Meeting details').get_attribute('href')
 
+    for element in All_Meetings.find_elements_by_link_text('Meeting details'): 
+        link = element.get_attribute('href')
         if link is not None:
-   
-   
-            get_agenda(link, Meeting_Date, Name)
+            break
     driver.close()
+    return(link)
 
-def get_agenda(link, meeting_date, meeting_name):
-    print(link)
-
+def get_agenda_table(link):
     header_list = [ 'File #', 'Date', 'Meeting type', 'Staff Report link', 'Ver.', 'Agenda #', 'Agenda Note', 'Type', 'Title', 'Action', 'Result', 'Action Details', 'Video' ]
 # this is a revised list of headers for the output csv file.
 
     tables = pd.read_html(link, keep_default_na=False)
     # sometimes generates ConnectionResetError
 
+    meeting_name = tables[1].iloc[0,1].replace('/', '_')
+# Grabbing meeting name from tables (for San Jose, tables[1] works, but need to make this dynamic for other cities with different table indexes)
+    target_date = (tables[2].iloc[0,1]).split(' ')[0].replace('/', '-')
+# Grabbing meeting date from tables (for San Jose, tables[2] works, but need to make this dynamic for other cities with different table indexes)
     last_table = len(tables) - 1
     agenda_table = tables[last_table].reindex(columns = header_list)
-    agenda_table['Date'] = meeting_date
+    agenda_table['Date'] = target_date
     agenda_table['Meeting type'] = meeting_name
 # This adds new columns to dataframe for the 'Staff Report link', 'Date' and 'Meeting type'
     agenda_table = agenda_table[agenda_table['File #'] != '']
@@ -122,8 +103,20 @@ def get_agenda(link, meeting_date, meeting_name):
                 continue
 
     agenda_table['Staff Report link'] = data
-    agenda_table.to_csv((full_path + meeting_name + '_' + meeting_date + '.csv'), index=False, errors='replace')
-# Creates filename with 'Meeting Name' and 'Date'
-    drive_upload(full_path, drive)
+    return agenda_table
 
-scrape_meetings(current_city)
+
+
+
+# Get the URL
+link = get_link()
+agenda_table = get_agenda_table(link)
+
+# Generate the appropriate csv
+meeting_type = agenda_table['Meeting type'].tolist()[0]
+target_date = agenda_table['Date'].tolist()[0]
+agenda_table.to_csv((full_path + meeting_type + '_' + target_date + '.csv'), index=False, errors='replace')
+
+# Now that we have the csv - upload to Google drive
+drive_upload(full_path, drive, current_city)
+
